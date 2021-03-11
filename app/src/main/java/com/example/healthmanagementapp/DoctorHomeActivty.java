f06d1697e7c3047e4eff119397066264e059c2e6
mania <sakshiprasad26@gmail.com>
2021-03-11 07:47:05 -0800

    Added home activity screen for all actors and desgined splash and homescreen

---
+++ app/src/main/java/com/example/healthmanagementapp/DoctorHomeActivty.java
@@ -0,0 +1,14 @@
+package com.example.healthmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorHomeActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_home_activty);
+    }
+}
