624b815dcdf612d8e79a1dcf1df7ee93f9295632
mania <sakshiprasad26@gmail.com>
2021-03-11 12:57:48 -0800

    Admin Firebase authentication implemented.

--- app/src/main/java/com/example/healthmanagementapp/MainActivity.java
+++ app/src/main/java/com/example/healthmanagementapp/MainActivity.java
@@ -3,12 +3,132 @@ package com.example.healthmanagementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton radAdmin, radPatient,radDoctor,radCashier;
+    EditText username,pass;
+    Button btnLogin, btnSignUp, btnALogin;
+    TextView register, resetPass;
+    LinearLayout layoutAdmin, layoutUser;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        btnALogin = findViewById(R.id.buttonAdminLogin);
+        btnSignUp = findViewById(R.id.buttonAdminSignup);
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        register = findViewById(R.id.textViewRegister);
+        resetPass = findViewById(R.id.textViewForgotPassword);
+
+        radAdmin = findViewById(R.id.radioButtonAdmin);
+        radPatient = findViewById(R.id.radioButtonPatient);
+        radDoctor = findViewById(R.id.radioButtonDoctor);
+        radCashier = findViewById(R.id.radioButtonCashier);
+
+        username = findViewById(R.id.editTextLoginUsername);
+        pass = findViewById(R.id.editTextLoginPassword);
+
+        layoutUser = findViewById(R.id.linearLayoutUser);
+        layoutAdmin = findViewById(R.id.linearLayoutAdmin);
+
+        layoutAdmin.setVisibility(View.GONE);
+        layoutUser.setVisibility(View.GONE);
+
+        radAdmin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(radAdmin.isChecked()){
+                    layoutAdmin.setVisibility(View.VISIBLE);
+                    layoutUser.setVisibility(View.GONE);
+                    btnALogin.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Login();
+                        }
+                    });
+                    btnSignUp.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Signup();
+                        }
+                    });
+                }
+                else if(radPatient.isChecked() || radDoctor.isChecked() || radCashier.isChecked()){
+                    layoutUser.setVisibility(View.VISIBLE);
+                    layoutAdmin.setVisibility(View.GONE);
+                }
+
+
+
+            }
+        });
+
+
+
+    }
+
+
+    public void Signup(){
+        String email = username.getText().toString();
+        String password = pass.getText().toString();
+        if (email.length() > 0 && password.length() > 0){
+            if(password.length()>=9){
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("TAG", "createUserWithEmail:success");
+                                FirebaseUser user = auth.getCurrentUser();
+                                Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                // Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(this, "There's an exiting account with same Email.",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+            else{
+                Toast.makeText(this, "Password should at least have 9 characters.", Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(this, "Oh no, seems like you missed a spot!", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void Login(){
+        String email = username.getText().toString();
+        String password = pass.getText().toString();
+        if(email.length() > 0 && password.length() > 0){
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+                if(task.isSuccessful()){
+                    Toast.makeText(this, "Successfully Logged In",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else{
+            Toast.makeText(this,"Please enter email and password!",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/healthmanagementapp/RegisterActivity.java
+++ app/src/main/java/com/example/healthmanagementapp/RegisterActivity.java
@@ -3,12 +3,46 @@ package com.example.healthmanagementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    LinearLayout layoutDoctor,layoutPatient;
+    ImageView imgDoc, imgPat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        layoutDoctor = findViewById(R.id.linearLayoutDoctor);
+        layoutPatient = findViewById(R.id.linearLayoutPatient);
+
+        layoutDoctor.setVisibility(View.GONE);
+        layoutPatient.setVisibility(View.GONE);
+
+        imgDoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutDoctor.setVisibility(View.VISIBLE);
+                layoutPatient.setVisibility(View.GONE);
+            }
+        });
+        imgPat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutDoctor.setVisibility(View.GONE);
+                layoutPatient.setVisibility(View.VISIBLE);
+            }
+        });
     }
+
 }
