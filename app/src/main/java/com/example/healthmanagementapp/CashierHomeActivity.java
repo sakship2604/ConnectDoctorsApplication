be5795d74451f4796a72b80d51c8203454e542d1
Uberizm <80417780+Uberizm@users.noreply.github.com>
2021-03-18 10:22:41 -0700

    Cashier Interface visual changes and exported the cashier home activity to allow for easier changes Logout Button added that temporarily sends you to MainActivity but should later work with the database to actually log the user out, not sure how this will work for cashier users though.

--- app/src/main/java/com/example/healthmanagementapp/CashierHomeActivity.java
+++ app/src/main/java/com/example/healthmanagementapp/CashierHomeActivity.java
@@ -2,13 +2,44 @@ package com.example.healthmanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CashierHomeActivity extends AppCompatActivity {
+
+    Button btnShow, btnPayment, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cashier_home);
+
+        btnShow = findViewById(R.id.buttonShowPayments);
+        btnLogout = findViewById(R.id.buttonCashierLogout);
+        btnPayment = findViewById(R.id.buttonPayment);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //would load the payment data here
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CashierHomeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //would use the patients email to notify/request payment from them
+            }
+        });
     }
 }
