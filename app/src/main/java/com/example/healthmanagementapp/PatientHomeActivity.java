4f588cf334af43fbea479666922c96774d2a9a70
sakship2604 <68474441+sakship2604@users.noreply.github.com>
2021-03-24 08:42:22 -0700

    Minimal changes.

--- app/src/main/java/com/example/healthmanagementapp/PatientHomeActivity.java
+++ app/src/main/java/com/example/healthmanagementapp/PatientHomeActivity.java
@@ -1,15 +1,42 @@
 package com.example.healthmanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import static com.example.healthmanagementapp.RegisterActivity.MyPREFERENCES;
 
 public class PatientHomeActivity extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, MODE_PRIVATE);
+        if (sharedPreferences.getInt("admin", 0) == 1) {
+            Intent intent = new Intent(this, ResetPasswordActivity.class);
+            startActivity(intent);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("admin", 0);
+            editor.apply();
+        }
+    }
+
+// for back navigation
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
     public void goToFindDoctor(View view) {
         Intent intent = new Intent(PatientHomeActivity.this, AddQueryActivity.class);
